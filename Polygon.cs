b0f0d280b49da8cb485afa246758e107f74f0b42
straussdave <davidstrauss002@gmai.com>
2024-11-29 13:21:34 +0100

    gabriel changes

---
+++ Polygon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Point = System.Windows.Point;
+
+namespace PolygonDraw
+{
+    internal class Polygon
+    {
+        private Stack<Point> _displayedPoints = new Stack<Point>();
+        private Stack<Point> _removedPoints = new Stack<Point>();
+
+        public Polygon(Point startPoint)
+        {
+            _displayedPoints.Push(startPoint);
+        }
+
+
+        public void Undo()
+        {
+
+            Point lastAddedPoint = _displayedPoints.Pop();
+            if (lastAddedPoint != null)
+            {
+                _removedPoints.Push(lastAddedPoint);
+            }
+        }
+
+        public void Redo()
+        {
+            Point lastAddedPoint = _removedPoints.Pop();
+            if (lastAddedPoint != null)
+            {
+                _displayedPoints.Push(lastAddedPoint);
+            }
+        }
+
+        public void AddPoint(Point point)
+        {
+            _displayedPoints.Push(point);
+        }
+    }
+}
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using CommunityToolkit.Mvvm.ComponentModel;
-
+using Point = System.Windows.Point;
 
 namespace PolygonDraw
 {
@@ -11,11 +13,65 @@ namespace PolygonDraw
         [ObservableProperty]
         string? mousePosition;
 
-        public void OnMouseMove(object sender, MouseEventArgs e)
+        Stack<Polygon>? polygons = new Stack<Polygon>();
+
+        bool currentlyEditing = false;
+
+        //public void OnMouseMove(object sender, MouseEventArgs e)
+        //{
+        //    var position = Mouse.GetPosition(Application.Current.MainWindow);
+        //    MousePosition = $"X: {position.X}, Y: {position.Y}";
+        //}
+
+        public void OnClick(object sender, MouseEventArgs e)
         {
-            var position = Mouse.GetPosition(Application.Current.MainWindow);
-            MousePosition = $"X: {position.X}, Y: {position.Y}";
+            Point currentPosition = Mouse.GetPosition(Application.Current.MainWindow);
+            MousePosition = $"X: {currentPosition.X}, Y: {currentPosition.Y}";
+            if (currentlyEditing)
+            {
+                Polygon currentPolygon = polygons.Peek();
+                if (currentPolygon != null)
+                {
+                    currentPolygon.AddPoint(currentPosition);
+                }
+                else
+                {
+                    polygons.Push(new Polygon(currentPosition));
+                }
+            }
+            else
+            {
+                polygons.Push(new Polygon(currentPosition));
+                currentlyEditing = true;
+            }
         }
+
+        public void OnDoubleClick(object sender, MouseEventArgs e)
+        {
+            MousePosition = "Double Clicked";
+            currentlyEditing = false;
+        }
+
+        public void Undo()
+        {
+            Polygon currentPolygon = polygons.Peek();
+            if (currentPolygon != null)
+            {
+                currentPolygon.Undo();
+            }
+        }
+
+        public void Redo()
+        {
+            Polygon currentPolygon = polygons.Peek();
+            if (currentPolygon != null)
+            {
+                currentPolygon.Redo();
+            }
+
+        }
+
+
     }
 }
 
